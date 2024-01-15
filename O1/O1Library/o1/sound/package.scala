65d89eef4f1518cad5189f591ed6d50d9a70d828
Leo Koivisto <145397691+LeoKoivisto@users.noreply.github.com>
2024-01-15 15:48:49 +0200

    Delete O1 directory

--- O1/O1Library/o1/sound/package.scala
+++
@@ -1,9 +0,0 @@
-/** Please see one of the two subpackages:
-  *
-  *  - [[o1.sound.midi]] for MIDI sound described in terms of notes,
-  *    instruments, and other directives
-  *
-  *  - [[o1.sound.sampled]] for working with recorded sound samples. */
-package o1.sound
-
-private class nowarn
